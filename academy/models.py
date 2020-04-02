e2a9c33e98afb14ffc4ac784f2bf7091916bc935
DanielGonzalez <jodgonzalezvi@unal.edu.co>
2020-04-02 16:36:41 -0500

    First models

--- academy/models.py
+++ academy/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+
+class Course(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    abstract = models.TextField()
+
+
+def files_path(instance, filename):
+    return f"{instance.course.topic.name}/{instance.course.name}/{instance.name}"
+
+
+class Document(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    file = models.FileField(upload_to=files_path)
