c22d566ecae5e8b94a6617a32fc367b0f6d678bb
DanielGonzalez <jodgonzalezvi@unal.edu.co>
2020-04-02 16:24:50 -0500

    Installing academy app

--- Tutorials/settings.py
+++ Tutorials/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
+    'rest_framework',
+    'django_extensions',
+    'django_filters',
+    'academy',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
