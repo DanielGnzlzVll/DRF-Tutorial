88e4e4e53bcdacee67ca046e97ffc5d8bcf1f0ca
DanielGonzalez <jodgonzalezvi@unal.edu.co>
2020-04-03 00:52:40 -0500

    Create admin

--- academy/admin.py
+++ academy/admin.py
@@ -1,3 +1,38 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import Topic, Course, Document
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+    search_fields = ('name',)
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'topic',
+        'name',
+        'abstract',
+        'created_date',
+        'updated_date',
+    )
+    list_filter = ('topic', 'created_date', 'updated_date')
+    search_fields = ('name',)
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'course',
+        'name',
+        'file',
+        'created_date',
+        'updated_date',
+    )
+    list_filter = ('course', 'created_date', 'updated_date')
+    search_fields = ('name',)
