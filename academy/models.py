4c247d8aaed311a58e40afc1b67aeb7e8778e3a4
DanielGonzalez <jodgonzalezvi@unal.edu.co>
2020-04-03 00:52:13 -0500

    Improving models

--- academy/models.py
+++ academy/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Topic(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     description = models.TextField()
 
 
@@ -10,6 +10,12 @@ class Course(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     abstract = models.TextField()
+
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = [["topic", "name"],]
 
 
 def files_path(instance, filename):
@@ -20,3 +26,9 @@ class Document(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     file = models.FileField(upload_to=files_path)
+
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = [["course", "name"],]
